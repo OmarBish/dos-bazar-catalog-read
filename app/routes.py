1ea254f0ea4b556e815a56285ac2ca83368b01bd
OmarBish <omar.f.bishtawi@gmail.com>
2020-05-17 16:48:45 +0300

    pass insert/update to master and recive updates from master

--- app/routes.py
+++ app/routes.py
@@ -13,9 +13,7 @@ from app import app
 def index():
     return redirect('https://bazar2.docs.apiary.io')
 
-
-# index route, redirect to api dcumentation url
-@app.route('/query',methods=['POST'])
+@app.route('/master-notification',methods=['POST'])
 def query():
     # variables
     data = request.get_json()
@@ -33,10 +31,35 @@ def query():
         cursor.execute(sqlite_query)
         records = cursor.fetchall()
         conn.commit()
-        res ={
-            'id':cursor.lastrowid
-        } 
         status = 201
+    elif sqlite_query.startswith('UPDATE'):
+        cursor.execute(sqlite_query)
+        conn.commit()
+        status = 201
+    # close connection and send data
+    cursor.close()
+    conn.close()
+    return jsonify() , status
+
+
+# index route, redirect to api dcumentation url
+@app.route('/query',methods=['POST'])
+def query():
+    # variables
+    data = request.get_json()
+    res = None
+    status = None
+
+    # connect to db
+    sqlite_query = data['sqlite_query']
+    conn = sqlite3.connect('bazar.db')
+    cursor = conn.cursor()
+    
+    # operation
+    if sqlite_query.startswith('INSERT') :
+        master_res = requests.post(replica + "/query" ,json=data)
+        res = master_res.get_json()
+        status = master_res.status_code
     elif sqlite_query.startswith('SELECT'):
         cursor.execute(sqlite_query)
         records = cursor.fetchall()
@@ -45,12 +68,9 @@ def query():
             res.append({'id':row[0],'title':row[1],'amount':row[2]})
         status = 200
     elif sqlite_query.startswith('UPDATE'):
-        cursor.execute(sqlite_query)
-        conn.commit()
-        res ={
-            'id':cursor.lastrowid
-        } 
-        status = 201
+        master_res = requests.post(replica + "/query" ,json=data)
+        res = master_res.get_json()
+        status = master_res.status_code
     else:
         res={
             'message':'unsupported operation'
